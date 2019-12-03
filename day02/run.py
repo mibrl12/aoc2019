c94bfe385083aac72ce3d940c1a13aabc236d196
mibrl12 <mikalai.syty@gmail.com>
2019-12-03 17:59:02 +0100

    Implements task 2 of the day 02

--- day02/run.py
+++ day02/run.py
@@ -1,6 +1,11 @@
+from typing import List
 
-def run_intcode(intcode_str: str) -> str:
+
+def run_intcode(intcode_str: str, first=12, second=2) -> List[int]:
     opcodes = [int(opcode) for opcode in intcode_str.split(',')]
+
+    opcodes[1] = first
+    opcodes[2] = second
 
     i = 0
     while i < len(opcodes):
@@ -12,8 +17,21 @@ def run_intcode(intcode_str: str) -> str:
             opcodes[opcodes[i + 3]] = opcodes[opcodes[i + 1]] * opcodes[opcodes[i + 2]]
         i += 4
 
-    return ','.join(map(str, opcodes))
+    return opcodes
+
+
+def find_target_pair(intcode_str: str, target_output: int):
+    for noun in range(100):
+        for verb in range(100):
+            output = run_intcode(intcode_str, noun, verb)
+            if output[0] == target_output:
+                return noun, verb
 
 
 with open('input.txt') as f:
-    print(f'Day 02. Part 1: \n{run_intcode(f.readline())}')
+    intcode_str = f.readline()
+    day_1_solution = ",".join(map(str, run_intcode(intcode_str)))
+    print(f'Day 02. Part 1: \n{day_1_solution}')
+    target_pair = find_target_pair(intcode_str, target_output=19690720)
+    day_2_solution = 100 * target_pair[0] + target_pair[1]
+    print(f'Day 02. Part 2: \n{day_2_solution}')
