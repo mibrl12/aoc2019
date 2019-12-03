02950fb1b1b5fc8149c7782f5d3636371b54783d
mibrl12 <mikalai.syty@gmail.com>
2019-12-03 21:14:22 +0100

    Implements task 2 of the day 03

--- day03/run.py
+++ day03/run.py
@@ -5,6 +5,11 @@ def calculate_distance(x, y) -> int:
     return abs(0 - x) + abs(0 - y)
 
 
+def find_intersections(
+        points_wire_1: List[Tuple[int, int]], points_wire_2: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    return list(set(points_wire_1) & set(points_wire_2))
+
+
 def calculate_manhattan_distance(wire_1: str, wire_2: str) -> int:
     points_wire_1 = get_wire_points(wire_1.split(','))
     points_wire_2 = get_wire_points(wire_2.split(','))
@@ -23,6 +28,35 @@ def calculate_manhattan_distance(wire_1: str, wire_2: str) -> int:
             closest_intersection_dist = intersection_dist
 
     return closest_intersection_dist
+
+
+def calculate_steps_to_intersection(points: List[int], intersection: Tuple[int, int]) -> int:
+    for i, point in enumerate(points):
+        if point == intersection:
+            return i + 1
+    return -1
+
+
+def calculate_closest_intersection(wire_1: str, wire_2: str) -> int:
+    points_wire_1 = get_wire_points(wire_1.split(','))
+    points_wire_2 = get_wire_points(wire_2.split(','))
+
+    intersections = find_intersections(points_wire_1, points_wire_2)
+
+    closest_distance_to_intersection = calculate_steps_to_intersection(points_wire_1, intersections[0]) + \
+                                       calculate_steps_to_intersection(points_wire_2, intersections[0])
+
+    for i, intersection in enumerate(intersections):
+        if i == 0:
+            continue
+
+        distance_to_intersection = calculate_steps_to_intersection(points_wire_1, intersections[i]) + \
+                                   calculate_steps_to_intersection(points_wire_2, intersections[i])
+
+        if distance_to_intersection < closest_distance_to_intersection:
+            closest_distance_to_intersection = distance_to_intersection
+
+    return closest_distance_to_intersection
 
 
 def get_wire_points(steps_wire_1):
@@ -59,13 +93,10 @@ def get_path_points(x, y, direction: str, distance: int) -> List[Tuple[int, int]
     return path_points
 
 
-def find_intersections(
-        points_wire_1: List[Tuple[int,int]], points_wire_2: List[Tuple[int,int]]) -> List[Tuple[int,int]]:
-    return list(set(points_wire_1) & set(points_wire_2))
-
-
 with open('input.txt') as f:
     wire_1 = f.readline()
     wire_2 = f.readline()
     day_1_solution = calculate_manhattan_distance(wire_1, wire_2)
     print(f'Day 03. Part 1: \n{day_1_solution}')
+    day_2_solution = calculate_closest_intersection(wire_1, wire_2)
+    print(f'Day 03. Part 2: \n{day_2_solution}')
--- day03/tests.py
+++ day03/tests.py
@@ -1,16 +1,22 @@
-from day03.run import calculate_manhattan_distance
-
+from day03.run import calculate_manhattan_distance, calculate_closest_intersection
 
 assert calculate_manhattan_distance(
     wire_1='R8,U5,L5,D3',
     wire_2='U7,R6,D4,L4') == 6
 
+test_1_wire_1_str = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'
+test_1_wire_2_str = 'U62,R66,U55,R34,D71,R55,D58,R83'
 
-assert calculate_manhattan_distance(
-    wire_1='R75,D30,R83,U83,L12,D49,R71,U7,L72',
-    wire_2='U62,R66,U55,R34,D71,R55,D58,R83') == 159
+test_2_wire_1_str = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51'
+test_2_wire_2_str = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+
+assert calculate_manhattan_distance(wire_1=test_1_wire_1_str, wire_2=test_1_wire_2_str) == 159
 
 
-assert calculate_manhattan_distance(
-    wire_1='R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
-    wire_2='U98,R91,D20,R16,D67,R40,U7,R15,U6,R7') == 135
+assert calculate_manhattan_distance(wire_1=test_2_wire_1_str, wire_2=test_2_wire_2_str) == 135
+
+
+assert calculate_closest_intersection(wire_1=test_1_wire_1_str, wire_2=test_1_wire_2_str) == 610
+
+
+assert calculate_closest_intersection(wire_1=test_2_wire_1_str, wire_2=test_2_wire_2_str) == 410
